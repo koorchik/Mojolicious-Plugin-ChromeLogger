bd446b7cdfd04fa712314bb0f1671013a8884772
Viktor Turskyi <koorchik@gmail.com>
2013-06-27 17:05:37 +0300

    with caller info now

--- lib/Mojolicious/Plugin/ChromeLogger.pm
+++ lib/Mojolicious/Plugin/ChromeLogger.pm
@@ -4,9 +4,11 @@ use Mojo::Base 'Mojolicious::Plugin';
 use Mojo::ByteStream qw/b/;
 use Mojo::JSON;
 
-our $VERSION = 0.01;
+use Mojo::Log::ChromeLogger;
 
-has logs => sub { return [] };
+our $VERSION = 0.02;
+
+has 'chrome_log';
 
 my %types_map = (
     'debug' => 'log',
@@ -19,20 +21,14 @@ my %types_map = (
 sub register {
     my ( $self, $app ) = @_;
 
-    # override Mojo::Log->log
-    no strict 'refs';
-    my $stash = \%{"Mojo::Log::"};
-    my $orig  = delete $stash->{"log"};
-
-    *{"Mojo::Log::log"} = sub {
-        push @{ $self->logs }, [ $_[1], $_[-1] ];
-        $orig->(@_);
-    };
+    $self->chrome_log( Mojo::Log::ChromeLogger->new() );
+    $app->log( $self->chrome_log );
 
     $app->hook(
         after_dispatch => sub {
             my ($c) = @_;
-            my $logs = $self->logs;
+            my $logs = $self->chrome_log->history;
+            $self->chrome_log->history([]);
 
             # Leave static content untouched
             return if $c->stash('mojo.static');
@@ -41,7 +37,7 @@ sub register {
             return if $c->app->mode ne 'development';
 
             my $data = {
-                version => '0.01',
+                version => $VERSION,
                 columns => [ 'log', 'backtrace', 'type' ],
                 rows    => []
             };
@@ -49,14 +45,13 @@ sub register {
             # Logs: fatal, info, debug, error
             foreach my $msg (@$logs) {
                 push @{ $data->{rows} },
-                  [ $msg->[1], undef, $types_map{ $msg->[0] } ];
+                  [ $msg->[1], $msg->[2], $types_map{ $msg->[0] } ];
             }
 
             my $json       = Mojo::JSON->new()->encode($data);
             my $final_data = b($json)->encode('UTF-8')->b64_encode('');
             $c->res->headers->add( 'X-ChromeLogger-Data' => $final_data );
 
-            $self->logs( [] );
         }
     );
 }
